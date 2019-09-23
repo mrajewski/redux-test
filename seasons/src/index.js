ae85a2997f3803dac9398c92965b0a4e33053d19
mrajewski <rajewski.ma@gmail.com>
2019-09-23 18:22:33 +0200

    seasons app

---
+++ seasons/src/index.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from "./seasonDisplay";
+
+class App extends Component{
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>console.log(position),
+            (err)=> console.log(err)
+        );
+        return <div>Latitude: </div>
+    }
+
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
---
+++ seasons/src/seasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SeasonDisplay(){
+    return
+}
+
+export default SeasonDisplay
