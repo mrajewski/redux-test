8af9ecec5f4b8652f7ac2dea79376f304589083c
mrajewski <rajewski.ma@gmail.com>
2019-10-14 16:23:49 +0200

    statement

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,15 +1,40 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./seasonDisplay";
 
-class App extends Component{
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            lat: null,
+            errMsg: ''
+        };
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat:position.coords.latitude});
+            },
+            err => {
+                this.setState({errMsg:err.message});
+            }
+        );
+    }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position)=>console.log(position),
-            (err)=> console.log(err)
-        );
-        return <div>Latitude: </div>
+        if (this.state.errMsg && !this.state.lat){
+            return (
+                <div>
+                    Error: {this.state.errMsg}
+                </div>
+            )
+        }
+        if (!this.state.errMsg && this.state.lat){
+            return (
+                <div>
+                    Latitude: {this.state.lat}
+                </div>
+            )
+        }
+       return <div>Loading!</div>
     }
 
 }
