dd023efdef10e054bb7c986bbab2fe1bef3432bf
mrajewski <rajewski.ma@gmail.com>
2020-04-14 14:24:52 +0200

    new action wrapper for unique users rquests

--- posts/src/components/UserHeader.jsx
+++ posts/src/components/UserHeader.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import {fetchUser} from "../actions";
 import {connect} from "react-redux";
 
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId)
-    }
-
     render() {
         const {user} = this.props;
 
@@ -28,6 +23,5 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(
-    mapStateToProps,
-    {fetchUser}
+    mapStateToProps
 )(UserHeader);
