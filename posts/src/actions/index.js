e9cdad542edb28456d25ad0cd868d27e3cdbfeac
mrajewski <rajewski.ma@gmail.com>
2020-04-14 14:31:47 +0200

    refactor with chain

--- posts/src/actions/index.js
+++ posts/src/actions/index.js
@@ -4,8 +4,16 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
 
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(id => dispatch(fetchUser(id)));
+    //primary version
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // alternative chain method for users
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
 };
 
 export const fetchPosts = () => async dispatch => {
